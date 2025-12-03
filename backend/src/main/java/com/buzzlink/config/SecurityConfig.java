ef22ef9009a0eb9da2a8aab88e7010016086562a
bolt162 <ash16299@gmail.com>
2025-12-03 02:35:24 -0800

    fixing CORS

--- backend/src/main/java/com/buzzlink/config/SecurityConfig.java
+++ backend/src/main/java/com/buzzlink/config/SecurityConfig.java
@@ -39,7 +39,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://54.193.63.14:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
